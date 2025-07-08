c4713833cac0873345c274fc6c2505431aaf6ff0
Yuki Maruyama <maruyama_y@1923.co.jp>
2025-07-08 17:25:18 +0900

    metrics: update help text for HTTP requests counter

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -5,7 +5,7 @@ import "github.com/prometheus/client_golang/prometheus"
 var HTTPRequests = prometheus.NewCounterVec(
 	prometheus.CounterOpts{
 		Name: "http_requests_total",
-		Help: "Total number of HTTP requests",
+		Help: "Total number of HTTP requests received",
 	},
 	[]string{"method", "endpoint"},
 )
